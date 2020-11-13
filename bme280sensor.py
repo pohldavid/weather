a0ed5b3a85c648888d9c81ef0b161cdf88c23645
Dave <pohl.david@gmail.com>
2020-11-13 05:06:19 -0600

    re-wrote 280sensor read module

---
+++ bme280sensor.py
@@ -0,0 +1,22 @@
+import bme280
+import smbus2
+from datetime import datetime
+from datetime import date
+
+port = 1
+address = 0x77 # Adafruit BME280 address. Other BME280s may be different
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus, address)
+
+
+def read():
+    reading = bme280.sample(bus,address)
+    day = date.today()
+    time = datetime.now().strftime("%H:%M:%S")
+    data = ({"temperature" : reading.temperature,
+             "humidity" : reading.humidity,
+             "pressure" : reading.pressure,
+             "day" : day, "time" : time})
+
+    return data
--- testmd/testmd.py
+++
@@ -1,7 +0,0 @@
-import markdown
-
-markdown.markdownFromFile(
-    input='mockup-page.md',
-    output='output.html',
-    encoding='utf8',
-)
