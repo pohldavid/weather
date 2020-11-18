15d5db062991bb23516c4d325b92f30564da6d40
Dave-Desktop <pohl.david@gmail.com>
2020-11-18 08:22:42 -0600

    fix

--- mybottle/system_date.py
+++ mybottle/system_date.py
@@ -5,4 +5,4 @@ import subprocess
 print(subprocess.check_output('date').decode('ascii').strip())
 
 
-print(subprocess.check_output('vcgencmd','measure_temp').decode('ascii').strip())
+print(subprocess.check_output(['vcgencmd','measure_temp']).decode('ascii').strip())
